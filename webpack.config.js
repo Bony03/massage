b96076e2df52b1f087f92dc30cd2855a6e12f0ab
Bohdan <bvorush@gmail.com>
2023-06-14 20:10:24 +0300

    Initialization project, done menu and main hero. Almost header finished

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+
+const devMode = mode === "development";
+
+const target = devMode ? "web" : "browserslist";
+const devtool = devMode ? "source-map" : undefined;
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "src", "index.html"),
+  }),
+  new MiniCssExtractPlugin({
+    filename: "[name].[contenthash].css",
+  }),
+];
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.[contenthash].js",
+    clean: true,
+  },
+  plugins,
+  module: {
+    rules: [
+      { test: /\.(html)$/, use: ["html-loader"] },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === "production" ? "asset" : "asset/resource",
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+  },
+};
